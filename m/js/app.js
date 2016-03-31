cae927349fd8f89ec857dd7f66d49da67d3a68ee
doudoujay <doudoujay@qq.com>
2016-03-31 23:09:00 +0800

    First page finished

---
+++ m/js/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by jay on 16/3/31.
+ */
+
+//    Initialize Swiper
+var swiper = new Swiper('.swiper-container', {
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    direction: 'vertical'
+});
