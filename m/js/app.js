98b4247d67ebbb3631c30f4dffa2c7fb799963b8
jay <doudoujay@qq.com>
2016-04-14 14:22:42 +0800

    add auto play animation

--- m/js/app.js
+++ m/js/app.js
@@ -12,29 +12,37 @@ isWeiXin = function (){
     }else{
         return false;
     }
-}
+};
 
 //Replay
 
 playWelcome = function () {
-    welcome = HYPE.documents['DYC-M-welcome']
+    welcome = HYPE.documents['DYC-M-welcome'];
     welcome.startTimelineNamed('main', welcome.kDirectionForward)
-}
+};
 playDowhat = function () {
-    dowhat = HYPE.documents['DYC-M-dowhat']
+    dowhat = HYPE.documents['DYC-M-dowhat'];
     dowhat.startTimelineNamed('main', dowhat.kDirectionForward)
-}
-playSpace = function () {
-    space = HYPE.documents['DYC-M-space']
-    space.startTimelineNamed('main', space.kDirectionForward)
-}
+};
+playPage2 = function () {
+    page2 = HYPE.documents['lunar-page2'];
+    page2.startTimelineNamed('main', dowhat.kDirectionForward)
+};
+playPage3 = function () {
+    page3 = HYPE.documents['lunar-page3'];
+    page3.startTimelineNamed('main', space.kDirectionForward)
+};
+playPage4 = function () {
+    page4 = HYPE.documents['lunar-page4'];
+    page4.startTimelineNamed('main', space.kDirectionForward)
+};
 playAbout = function () {
-    about = HYPE.documents['DYC-M-about']
+    about = HYPE.documents['DYC-M-about'];
     if (about.currentTimeInTimelineNamed('click-button') == 0) {
         about.startTimelineNamed('main', about.kDirectionForward)
     }
 
-}
+};
 
 
 
